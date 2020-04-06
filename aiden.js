e36cfe080e300b48fcb3941131dfb48d1afc0662
Michael Morahan <morahan.mike@gmail.com>
2020-04-05 21:35:10 -0600

    reduced comments

--- aiden.js
+++ aiden.js
@@ -1,4 +1,6 @@
 // The Taveling Sales Person Problem
+// ==================================
+
 // let cities = [chicago, denver, jacksonville, houston, LA, Memphis, NYC, Seattle, StPaul];
 // // let abv = [C, D, J, H, L, M, N, S, P];
 
@@ -159,112 +161,5 @@ for (let i = 0; i < comboArr.length; i++) {
         answer = tempOrder;
     }
 }
-console.log("combos ==== ", combos, "start ==== ", start, "comboArr ==== ", comboArr)
 console.log(answer, smallest)
 
-
-
-
-
-
-
-
-
-// let chicago = {
-//     d: 900,
-//     j: 1100,
-//     h: 1100,
-//     l: 2000,
-//     m: 500,
-//     n: 800,
-//     s: 2100,
-//     sp: 350
-// }
-
-// let denver = {
-//     c: 900,
-//     j: 1700,
-//     h: 1000,
-//     l: 1000,
-//     m: 1050,
-//     n: 1800,
-//     s: 1300,
-//     sp: 900
-// }
-
-// let jacksonville = {
-//     c: 1100,
-//     d: 1700,
-//     h: 900,
-//     l: 2400,
-//     m: 750,
-//     n: 950,
-//     s: 3000,
-//     sp: 1400
-// }
-
-// let houston = {
-//     c: 1100,
-//     d: 1000,
-//     j: 900,
-//     l: 1550,
-//     m: 500,
-//     n: 1600,
-//     s: 2300,
-//     sp: 1200
-// }
-
-// let LA = {
-//     c: 2000,
-//     d: 1000,
-//     j: 2400,
-//     h: 1550,
-//     m: 1800,
-//     n: 2800,
-//     s: 2100,
-//     sp: 1900
-// }
-
-// let Memphis = {
-//     c: 500,
-//     d: 1050,
-//     j: 750,
-//     h: 500,
-//     l: 1800,
-//     n: 1100,
-//     s: 2300,
-//     sp: 1050
-// }
-
-// let NYC = {
-//     c: 800,
-//     d: 1800,
-//     j: 950,
-//     h: 1600,
-//     m: 2800,
-//     l: 2800,
-//     s: 2900,
-//     sp: 1000
-// }
-
-// let Seattle = {
-//     c: 2100,
-//     d: 1300,
-//     j: 3000,
-//     h: 2300,
-//     m: 2300,
-//     n: 2900,
-//     l: 2100,
-//     sp: 1700
-// }
-
-// let StPaul = {
-//     c: 350,
-//     d: 900,
-//     j: 1400,
-//     h: 1200,
-//     m: 1050,
-//     n: 1000,
-//     l: 1900,
-//     s: 1700
-// }
